d73987ed23f4cc9cdd9579c9e2b6851ffc9319be
Ali Kamandlu <alikmndlu1@gmail.com>
2026-09-19 20:20:36 +0330

    fix: resolve Wails v2 options.Maximised constant spelling in main.go (v1.3.3)

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 		Height:            800,
 		MinWidth:          960,
 		MinHeight:         600,
-		WindowStartState:  options.Maximized,
+		WindowStartState:  options.Maximised,
 		DisableResize:     false,
 		Fullscreen:        false,
 		Frameless:         false,
